8f77f5a7552db57367766a1ae55691de2ea1fe0f
henninhm <henning.moller@vikenfiber.no>
2025-05-13 14:49:40 +0200

    Moodle Emne 3, Uke 2, Dag 2 (5)

---
+++ TallGjett/TallGjett/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace TallGjett
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Program();
+            var r = new Random();
+            int target = r.Next(1, 101); 
+            p.Gjett(target, 0);
+        }
+
+        public void Gjett(int target, int guess)
+        {
+            //Console.WriteLine("Test:" + target);
+            if (guess == 0)
+            {
+                Console.WriteLine("Gjettelek. Prøv å finne tallet.");
+                NyttTall(target);
+            }
+            else if (guess < target)
+            {
+                Console.WriteLine("For lavt. Prøv igjen.");
+                NyttTall(target);
+            }
+            else if (guess > target) 
+            {
+                Console.WriteLine("For høyt. Prøv igjen.");
+                NyttTall(target);
+            }
+            else
+            {
+                Console.WriteLine("Du fant tallet! \nVil du spille på nytt? Svar y eller n");
+                var answer = Console.ReadLine();
+                if (answer ==  "y" || answer == "Y" || answer == "Yes" || answer == "yes")
+                {
+                    var r = new Random();
+                    Gjett(r.Next(1, 101), 0);
+                }
+
+            }
+
+        }
+        public void NyttTall(int target)
+        {
+            var newGuess = Console.ReadLine();
+            bool tall = int.TryParse(newGuess, out int newGuessInt);
+            if (tall)
+            {
+                Gjett(target, newGuessInt);
+            } 
+            else
+            {
+                Console.WriteLine("Skriv et tall");
+                NyttTall(target);
+            }
+
+        }
+    }
+}
---
+++ UkjentKode/UkjentKode/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace UkjentKode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var range = 250;
+            var counts = new int[range];
+            int total = 0;
+            string text = "something";
+            while (!string.IsNullOrWhiteSpace(text))
+            {
+                text = Console.ReadLine();
+                foreach (var character in text ?? string.Empty)
+                {
+                    int temp = (int)character;
+                    if (temp > 96 && temp < 123)
+                    {
+                        temp = temp - 32;
+                        counts[temp]++;
+                    }
+                    else
+                    {
+                        counts[(int)character]++;
+                    } 
+                }
+                total += text.Length;
+                for (var i = 0; i < range; i++)
+                {
+                    if (counts[i] > 0)
+                    {
+                        var character = (char)i;
+                        double perc = (double)counts[i]*100/total;
+                        perc = Math.Round(perc, 2);
+                        string output = character + " - " + counts[i] + " - " + perc + "%";
+                        Console.CursorLeft = Console.BufferWidth - output.Length - 1;
+                        Console.WriteLine(output);
+                    }
+                }
+            }
+        }
+    }
+}
