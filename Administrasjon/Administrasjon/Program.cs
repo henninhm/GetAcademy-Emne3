f9733ecbcce5f2285bd1def58984fa7b12f68032
henninhm <henning.moller@vikenfiber.no>
2025-05-23 14:53:04 +0200

    Moodle Emne 3, Uke 3, Dag 5 (13)

--- Administrasjon/Administrasjon/Program.cs
+++ Administrasjon/Administrasjon/Program.cs
@@ -10,20 +10,55 @@
             en.SkrivUtInfo();
             to.SkrivUtInfo();
 
-            Fag ehs = new Fag("Introduksjon til Etthjulssykkel", "EHS100", 10);
+            Fag ehs = new Fag("Introduksjon til Etthjulssykkel", " EHS100", 10);
             Fag u2  = new Fag("How To Dismantle an Atomic Bomb", "U2-2004", 5);
             Fag sd  = new Fag("Safety Dance", "MWH1995", 10);
-            Fag sm  = new Fag("Soul Mining", "TT1983", 10);
-            Fag gb  = new Fag("Ghost Busting", "GB1984", 10);
+            Fag sm  = new Fag("Soul Mining", " TT1983", 10);
+            Fag gb  = new Fag("Ghost Busting", " GB1984", 10);
 
             ehs.SkrivUtInfo();
             u2.SkrivUtInfo();
 
-            Karakter testAB  = new Karakter(en, u2, 5);
+            List<Fag> fagliste = new List<Fag>();
+            fagliste.Add(ehs);
+            fagliste.Add(u2);
+            fagliste.Add(sd);
+            fagliste.Add(sm);
+            fagliste.Add(gb);
+
+            en.Fag = fagliste;
+            to.Fag = fagliste;
+
+            Karakter testAB = new Karakter(en, u2, 5);
             Karakter navnEHS = new Karakter(to, ehs, 6);
+
+            List <Karakter> karEn = new List<Karakter>
+            {new Karakter(en, u2, 5),
+            new Karakter(en, ehs, 3),
+            new Karakter(en, sd, 6),
+            new Karakter(en, sm, 4),
+            new Karakter(en, gb, 5) };
 
             testAB.SkrivUtInfo();
             navnEHS.SkrivUtInfo();
+
+            en.Karakterer = karEn;
+            en.SkrivUtKarakterer();
+
+            List<Karakter> karTo = new List<Karakter> {
+            new Karakter(to, ehs, 6),
+            new Karakter(to, u2, 2),
+            new Karakter(to, sd, 3),
+            new Karakter(to, sm, 6),
+            new Karakter(to, gb, 5) };
+
+            to.Karakterer = karTo;
+            to.SkrivUtKarakterer();
+
+            Console.WriteLine(en.OppdaterSnitt());
+            Console.WriteLine(to.OppdaterSnitt());
+
+            Console.WriteLine(en.OppdaterStudiePoeng());
         }
     }
 }
--- Administrasjon/Administrasjon/Student.cs
+++ Administrasjon/Administrasjon/Student.cs
@@ -6,6 +6,11 @@
         public int Alder;
         public string StudieProg;
         public int StudID;
+        public List<Fag> Fag;
+        public List<Karakter> Karakterer;
+        public float Snitt;
+        public int AntallFag;
+        public int StudiePoeng;
 
         public Student(string navn, int alder, string studieProg, int studID)
         {
@@ -23,6 +28,37 @@
             Console.WriteLine("ID:             " + StudID);
             Console.WriteLine("-----------------------------");
         }
-    }
 
+        public void SkrivUtKarakterer()
+        {
+            Console.WriteLine("Student: " + Navn);
+            foreach (Karakter k in Karakterer)
+            {
+                Console.WriteLine($"Fagkode: {k.Fag.Fagkode}   Karakter: {k.Karakterverdi}");
+            }
+            Console.WriteLine("+++++++++++++++++++++++++++++++");
+        }
+
+        public float OppdaterSnitt()
+        {
+            foreach (var k in Karakterer)
+            {
+                Snitt += k.Karakterverdi;
+                AntallFag++;
+            }
+            Snitt = Snitt / AntallFag;
+            return Snitt;
+        }
+
+        public int OppdaterStudiePoeng()
+        {
+            StudiePoeng = 0;
+            foreach (Fag f in Fag)
+            {
+                StudiePoeng += f.AntallStudiepoeng;
+            }
+
+            return StudiePoeng;
+        }
+    }
 }
